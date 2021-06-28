e13e552a76fcdc711d649411fc50729bcb811c48
Jonathan Reisdorf <jonathan.reisdorf@data4life.care>
2021-06-28 16:00:03 +0200

    📝 small comment improvements

--- src/custom/config.ts
+++ src/custom/config.ts
@@ -1,8 +1,9 @@
 /**
  * Override individual configuration values in this file.
  * The advantage over editing the values directly in the config folder
- * is that the files in the custom folder will unlikely be touched in the
- * future, so it is safe to make modifications to this file and assume
+ * when working with a forked repository is that the files in the custom
+ * folder will unlikely be touched by the upstream in the future,
+ * so it is safe to make modifications to this file and assume
  * that no merge conflicts will happen.
  *
  * All overrides must be part of the default export of this file.
