3c3d73ab4a40c3f849dda2d6f62b5961e7d66493
Jonathan Reisdorf <jonathan.reisdorf@data4life.care>
2021-06-30 12:50:42 +0200

    ✨ add support for transferable extensions

--- src/config/fhir.ts
+++ src/config/fhir.ts
@@ -4,3 +4,8 @@ export const FHIR_SUPPORTED_EXTENSION_BASE_URLS = CUSTOM.FHIR_SUPPORTED_EXTENSIO
   'http://hl7.org/fhir/StructureDefinition',
   'https://num-compass.science/fhir/StructureDefinition',
 ];
+
+export const FHIR_RESPONSE_TRANSFERRED_EXTENSION_KEYS = CUSTOM.FHIR_RESPONSE_TRANSFERRED_EXTENSION_KEYS ?? [
+  'CompassGeccoItem',
+  'CompassInterversionId',
+];
