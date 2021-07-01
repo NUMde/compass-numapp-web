318651648b08e9861fd2f614a7a8b137b19a81ed
Jonathan Reisdorf <jonathan.reisdorf@data4life.care>
2021-07-01 10:59:02 +0200

    ✨ add back arrows to legal views

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,17 +2,26 @@ import { FunctionalComponent, h } from '@stencil/core';
 
 type CardProps = {
   headline: string;
+  returnRoute?: string;
   wide?: boolean;
 };
 
-export const Card: FunctionalComponent<CardProps> = ({ headline, wide = false }, children) => (
+export const Card: FunctionalComponent<CardProps> = ({ headline, returnRoute, wide = false }, children) => (
   <div class={`c-card-wrapper ${wide ? 'c-card-wrapper--wide' : ''}`}>
     <d4l-card classes="card--text-center card--desktop card--no-padding">
       <div
         slot="card-header"
         class="c-card__header--segmented u-padding-horizontal--medium u-padding-vertical--normal"
       >
-        <h2>{headline}</h2>
+        <h2 class={{ 'c-card__header-title': true, 'c-card__header-title--arrowed': !!returnRoute }}>
+          {headline}
+        </h2>
+
+        {returnRoute && (
+          <stencil-route-link url={returnRoute} anchorClass="c-card__header-back">
+            <d4l-icon-arrow-back classes="icon--small" />
+          </stencil-route-link>
+        )}
       </div>
 
       <div class="u-padding-horizontal--medium u-padding-bottom--medium" slot="card-content">
--- src/components/legal/legal.tsx
+++ src/components/legal/legal.tsx
@@ -1,5 +1,6 @@
 import { Component, h, Prop } from '@stencil/core';
 import { Card } from 'components/card/card';
+import { ROUTES } from 'config';
 import stores from 'stores';
 
 @Component({
@@ -10,7 +11,10 @@ export class NumLegal {
 
   render() {
     return (
-      <Card headline={stores.i18n.t(`${this.namespace}.headline`)}>
+      <Card
+        headline={stores.i18n.t(`${this.namespace}.headline`)}
+        returnRoute={stores.auth.isAuthenticated ? ROUTES.DASHBOARD : ROUTES.ROOT}
+      >
         <p class="u-infotext" innerHTML={stores.i18n.t(`${this.namespace}.content`)} />
       </Card>
     );
